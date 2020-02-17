b1f12fe8ab6b8ea3e719e5a5fadb939cfba8dfb5
amlane <twelvexstring@gmail.com>
2020-02-17 17:33:01 -0600

    corrected notes

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -15,13 +15,16 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        self.vertices[vertex_id] = set()
+        if vertex_id in self.vertices:
+            print("WARNING: Vertex already exists")
+        else:
+            self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        if v1 in self.vertices and v2 is self.vertices:
+        if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
         else:
             raise IndexError("That vertex does not exist")
@@ -42,7 +45,7 @@ class Graph:
         # add the starting vertex_id to the queue
         q.enqueue(starting_vertex)
         # create an empty set to store visited nodes
-        visited = {}
+        visited = set()
         # while the queue is not empty...
         while q.size() > 0:
             # dequeue the first vertex
@@ -67,7 +70,7 @@ class Graph:
         # add the starting vertex_id to the queue
         s.push(starting_vertex)
         # create an empty set to store visited nodes
-        visited = {}
+        visited = set()
         # while the queue is not empty...
         while s.size() > 0:
             # dequeue the first vertex
@@ -141,16 +144,16 @@ if __name__ == '__main__':
     graph.add_vertex(5)
     graph.add_vertex(6)
     graph.add_vertex(7)
-    graph.add_edge(5, 3)
-    graph.add_edge(6, 3)
-    graph.add_edge(7, 1)
-    graph.add_edge(4, 7)
-    graph.add_edge(1, 2)
-    graph.add_edge(7, 6)
-    graph.add_edge(2, 4)
-    graph.add_edge(3, 5)
-    graph.add_edge(2, 3)
-    graph.add_edge(4, 6)
+    # graph.add_edge(5, 3)
+    # graph.add_edge(6, 3)
+    # graph.add_edge(7, 1)
+    # graph.add_edge(4, 7)
+    # graph.add_edge(1, 2)
+    # graph.add_edge(7, 6)
+    # graph.add_edge(2, 4)
+    # graph.add_edge(3, 5)
+    # graph.add_edge(2, 3)
+    # graph.add_edge(4, 6)
 
     '''
     Should print:
